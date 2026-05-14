e62f94f1347370f936e9637670d2f4a5abbf8e0a
Joan <nutsukpuijoan@gmail.com>
2026-05-15 01:30:53 +0200

    start

---
+++ app/Onboarding.tsx
@@ -0,0 +1,10 @@
+import "@/global.css";
+import { Text, View } from "react-native";
+
+export default function Onboarding() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">this is Onboarding</Text>
+    </View>
+  );
+}
