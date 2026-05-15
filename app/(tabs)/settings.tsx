60e46aecc0c876e7c6b31f76a250e9885171f402
Joan <nutsukpuijoan@gmail.com>
2026-05-15 02:49:28 +0200

    features:  update tabs , style tabs , correct erros

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,16 @@
-import { View, Text } from "react-native";
+import {Text} from "react-native";
 import React from "react";
 
+import {styled} from "nativewind"
+import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 const Settings = () => {
-  return (
-    <View>
-      <Text>first</Text>
-    </View>
-  );
+    return (
+        <SafeAreaView className="flex-1 p-5 bg-background">
+            <Text>Settings</Text>
+        </SafeAreaView>
+    );
 };
 
 export default Settings;
