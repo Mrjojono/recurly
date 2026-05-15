c03e02101247caf2a7621f0900c796abe013133b
Joan <nutsukpuijoan@gmail.com>
2026-05-15 03:08:34 +0200

    implements fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,9 +6,8 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 p-5 bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-7xl font-sans-extrabold text-primary">Home</Text>
+      <Text className="text-7xl font-bold text-primary">Home</Text>
       <Link
         href="/Onboarding"
         className="mt-4 rounded bg-primary text-white
